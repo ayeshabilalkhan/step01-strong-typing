5c6ceca2e062cc978cd8f650b39a45ee8e6c6659
ayeshabilalkhan <ayeshabilalkhan7@gmail.com>
2024-07-16 10:56:14 -0700

    first commit

---
+++ app.ts
@@ -0,0 +1,21 @@
+//Strongly typed syntax
+
+let a : string = "Pakistan";
+a = "USA";
+let b : number = 9;
+let c : boolean = true;
+
+//Type inference
+let e = "USA";
+let f = 10.9;
+f = 22;
+let g = false;
+g = true;
+// console.log(f);
+
+// Example:
+// let num1 = "Ayesha";
+
+let num1: number = 10
+num1 = "20"
+
